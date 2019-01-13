c7ccb9b28e1bcbc410ab67ded543e9ab8289afbd
Przemek Całka <przemekcalka1@gmail.com>
2019-01-13 20:24:51 +0100

    Created main app and business logic

---
+++ waluty/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace waluty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome  in app");
+        }
+    }
+}
