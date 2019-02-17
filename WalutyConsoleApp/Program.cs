0e9f83ba388b7dcdc7f89f1af98faf13b656df96
Tadeusz Derkacz <tadeusz.derkacz@gmail.com>
2019-02-17 18:10:43 +0100

    JCSZ1FUSY-5 currencies comparator integrated in Menu

--- WalutyConsoleApp/Program.cs
+++ WalutyConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Collections.Generic;
 using WalutyBusinessLogic.LoadingFromFile;
 using System.Globalization;
+using WalutyBusinessLogic.CurrenciesComparision;
 
 namespace Console_Menu
 {
@@ -16,6 +17,7 @@ namespace Console_Menu
                 item1 = "SHOW ENTRIES FROM DATA RANGE",
                 item2 = "SHOW CURRENCIES IN PARTICULAR DAY",
                 item3 = "SHOW SUPPORTED CURRENCIES",
+                item4 = "COMPARE CURRENCIES",
                 exit = "EXIT";
         }
 
@@ -25,6 +27,7 @@ namespace Console_Menu
                 MenuItem.item1,
                 MenuItem.item2,
                 MenuItem.item3,
+                MenuItem.item4,
                 MenuItem.exit
             };
 
@@ -54,6 +57,13 @@ namespace Console_Menu
                     case MenuItem.item3:
                         drawSubMenuBody(MenuItem.item3);
                         ShowSupportedCurencies();
+                        drawSubMenuHelper();
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case MenuItem.item4:
+                        drawSubMenuBody(MenuItem.item4);
+                        CompareTwoCurrenciesBySingleDate();
                         drawSubMenuHelper();
                         Console.ReadKey();
                         Console.Clear();
@@ -339,6 +349,72 @@ namespace Console_Menu
             {
                 Console.WriteLine($"Date= {filteredRecord.Date} Open= {filteredRecord.Open} High= {filteredRecord.High} Low= {filteredRecord.Low} Close= {filteredRecord.Close}");
             }
+        }
+
+        public static void CompareTwoCurrenciesBySingleDate()
+        {
+
+            var _inputCurrency1 = "GBP.txt";
+            var _inputCurrency1Txt = "GBP";
+
+            var _inputCurrency2 = "AUD.txt";
+            var _inputCurrency2Txt = "AUD";
+
+            var _CurrentDate = "";
+            var _startDate = "";
+            var _endDate = "";
+
+            Loader loader = new Loader();
+            var getCurrency1 = loader.LoadCurrencyFromFile("GBP.txt");
+            var getCurrency2 = loader.LoadCurrencyFromFile("AUD.txt");
+
+            Console.WriteLine();
+            Console.WriteLine("Supported currencies : ");
+            ShowSupportedCurencies();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Enter first currency : ");
+            _inputCurrency1 = Console.ReadLine().ToUpper();
+            _inputCurrency1Txt = String.Concat(_inputCurrency1.ToUpper(), ".txt");
+            getCurrency1 = loader.LoadCurrencyFromFile(_inputCurrency1Txt);
+
+            Console.WriteLine();
+            Console.Write("Enter second currency : ");
+            _inputCurrency2 = Console.ReadLine().ToUpper();
+            _inputCurrency2Txt = String.Concat(_inputCurrency2.ToUpper(), ".txt");
+            getCurrency2 = loader.LoadCurrencyFromFile(_inputCurrency2Txt);
+
+            // pytanie czy obie waluty maja te same zakresy dat
+            _startDate = getCurrency1.ListOfRecords.Min(x => x.Date).ToString();
+            _endDate = getCurrency1.ListOfRecords.Max(x => x.Date).ToString();
+
+            Console.WriteLine($"For {_inputCurrency1} we have the range of dates you can take: ");
+            Console.WriteLine();
+            Console.WriteLine($"Start date : {_startDate.ToString()} End date : {_endDate} ");
+            //Console.WriteLine($"Start date : {_startDate.ToString("MMMM dd, yyyy", "pl-PL")} End date : {_endDate} ");
+
+            Console.WriteLine();
+
+            Console.WriteLine();
+            Console.Write("Enter date for currencies: ");
+            _CurrentDate = Console.ReadLine();
+
+            CurrenciesComparator currencies = new CurrenciesComparator();
+            Console.WriteLine(currencies.CompareCurrencies(_inputCurrency1, _inputCurrency2, Convert.ToInt32(_CurrentDate)));
+
+            //List<CurrencyRecord> listOfFilteredRecords = new List<CurrencyRecord>();
+            //foreach (var record in getCurrency.ListOfRecords)
+            //{
+            //    if (record.Date > Convert.ToInt32(_startDate) && record.Date == Convert.ToInt32(_endDate))
+            //    {
+            //        listOfFilteredRecords.Add(record);
+            //    }
+            //}
+            //foreach (var filteredRecord in listOfFilteredRecords)
+            //{
+            //    Console.WriteLine($"Date= {filteredRecord.Date} Open= {filteredRecord.Open} High= {filteredRecord.High} Low= {filteredRecord.Low} Close= {filteredRecord.Close}");
+            //}
         }
 
         public static void ShowSupportedCurencies()
