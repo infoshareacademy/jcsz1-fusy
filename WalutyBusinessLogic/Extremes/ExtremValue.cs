2c85ee534b0ff6d48bf23171433b3318821ac41e
Przemek Całka <przemekcalka1@gmail.com>
2019-03-17 19:41:07 +0100

    Separation class ExtremValue

---
+++ WalutyBusinessLogic/Extremes/ExtremValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalutyBusinessLogic.Extremes
+{
+    public class ExtremeValue
+    {
+        public float MaxValue { get; set; }
+        public float MinValue { get; set; }
+
+        public override string ToString()
+        {
+            return $"Max {MaxValue} ; Min {MinValue}";
+        }
+    }
+}
