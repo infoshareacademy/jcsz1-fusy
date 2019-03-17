1e18e000c36e2ec02773a2896074d8a0c0b9c8a0
Przemek Całka <przemekcalka1@gmail.com>
2019-03-17 19:42:49 +0100

    Search local and global extreme finished

--- WalutyBusinessLogic/Extremes/GlobalExtremes.cs
+++ WalutyBusinessLogic/Extremes/GlobalExtremes.cs
@@ -1,50 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using WalutyBusinessLogic.CurrencyConversion;
-using  WalutyBusinessLogic.LoadingFromFile;
+using WalutyBusinessLogic.LoadingFromFile;
 
-namespace WalutyBusinessLogic.GlobaExtremes
+namespace WalutyBusinessLogic.Extremes
 {
     public class GlobalExtremes
     {
 
-        public ExtremeValue GetGlobaExtreme(string nameCurrency)
+        public ExtremeValue GetGlobalExtreme(string nameCurrency)
         {
             ExtremeValue extremeValue = new ExtremeValue();
-            nameCurrency += ".txt";
             Loader loader = new Loader();
-            CurrencyRecord currencyRecord = new CurrencyRecord();
             Currency currency = loader.LoadCurrencyFromFile(nameCurrency);
             List<CurrencyRecord> listOfRecords = currency.ListOfRecords;
             extremeValue.MaxValue = listOfRecords.Max(c=> c.High);
             extremeValue.MinValue = listOfRecords.Min(c => c.Low);
             return extremeValue;
         }
-
-    }
-
-    class LocalExtremes
-    {
+   
         public ExtremeValue GetLocalExtreme(string nameCurrency, int date, int date2)
         {
             ExtremeValue extremeValue = new ExtremeValue();
-            nameCurrency += ".txt";
             Loader loader = new Loader();
-            CurrencyRecord currencyRecord = new CurrencyRecord();
             Currency currency = loader.LoadCurrencyFromFile(nameCurrency);
             List<CurrencyRecord> listOfRecords = currency.ListOfRecords;
-            var e = listOfRecords.Select(c => c.Date > date && c.Date < date2 )
-                                                 .Max(c => c.);
+
+            extremeValue.MaxValue = listOfRecords.Where(c => c.Date >= date && c.Date <= date2 )
+                .Max(c => c.High);
+
+            extremeValue.MinValue = listOfRecords.Where(c => c.Date >= date && c.Date <= date2)
+                .Min(c => c.Low);
+
             return extremeValue;
         }
-    }
-    
 
-    public class ExtremeValue
-    {
-        public float MaxValue { get; set; }
-        public float MinValue { get; set; }
     }
+
+
+    
 }
