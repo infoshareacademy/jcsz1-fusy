2555fcea667cfc8f0560aa89c9fa322a06a21573
Przemek Całka <przemekcalka1@gmail.com>
2019-03-18 01:04:21 +0100

    Delete file CurrencyForConversation, separation method GetDesiredCurrency, fix method ConvertCurrency

--- WalutyBusinessLogic/CurrencyConvertion/CurrencyConvertion.cs
+++ WalutyBusinessLogic/CurrencyConvertion/CurrencyConvertion.cs
@@ -1,44 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using WalutyBusinessLogic.CurrencyConversion;
 using WalutyBusinessLogic.LoadingFromFile;
 
 namespace WalutyBusinessLogic
 {
-    class CurrencyConvertion
+    public class CurrencyConvertion
     {
-        CurrencyRecord currencyRecord = new CurrencyRecord();
-        CurrencyForConversation firstCurrencyConvertion = new CurrencyForConversation();
-        CurrencyForConversation secondCurrencyConvertion = new CurrencyForConversation();
+        private float ValueFirstCurrency { get; set; }
+        private float AmountFirstCurrency { get; set; }
+        private float ValueSecondCurrency { get; set; }
+        private float AmountSecondCurrency { get; set; }
+        
+        public float ConvertCurrency(float amountFirstCurrency, string firstNameCurrency,
+            string secondNameCurrency, int date)
+        {
+            AmountFirstCurrency = amountFirstCurrency;
+            CurrencyRecord FirstDesiredCurrency = GetDesiredCurrency(firstNameCurrency, date);
+            ValueFirstCurrency = FirstDesiredCurrency.Close;
+            CurrencyRecord SecondDesiredCurrency = GetDesiredCurrency(secondNameCurrency, date);
+            ValueSecondCurrency = SecondDesiredCurrency.Close;
+            AmountSecondCurrency = amountFirstCurrency * ValueFirstCurrency / ValueSecondCurrency;
+            return AmountSecondCurrency;
+        }
 
-        public List<CurrencyForConversation> ConvertCurrency(string nameCurrency, string secondNameCurrency, 
-            float amountCurrency, int date)
+        private CurrencyRecord GetDesiredCurrency(string nameCurrency,int  date)
         {
             Loader loader = new Loader();
             Currency currency = loader.LoadCurrencyFromFile(nameCurrency);
             List<CurrencyRecord> listOfRecords = currency.ListOfRecords;
-            CurrencyRecord desiredRecord = listOfRecords.Single(record => record.Date == date);
-            var currencyConvertionList = new List<CurrencyForConversation>();
-
-            firstCurrencyConvertion.Value = desiredRecord.Volume;
-            firstCurrencyConvertion.Amount = amountCurrency;
-            firstCurrencyConvertion.Name = nameCurrency;
-            currencyConvertionList.Add(firstCurrencyConvertion);
-
-            currency = loader.LoadCurrencyFromFile(secondNameCurrency);
-            listOfRecords = currency.ListOfRecords;
-
-            desiredRecord = listOfRecords.Single(record => record.Date == date);
-
-            secondCurrencyConvertion.Name = secondNameCurrency;
-            secondCurrencyConvertion.Value = currencyRecord.Volume;
-
-            secondCurrencyConvertion.Amount = firstCurrencyConvertion.Amount * firstCurrencyConvertion.Value /
-                                                   secondCurrencyConvertion.Value;
-            currencyConvertionList.Add(secondCurrencyConvertion);
-
-            return currencyConvertionList;
+            CurrencyRecord desiredRecord = listOfRecords.SingleOrDefault(record => record.Date == date);
+            return desiredRecord; 
         }
     }
 }
