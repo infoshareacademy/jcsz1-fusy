7bd33d2f3efe7c0c9c5bade0e85c48e9744d0c41
Przemek Całka <przemekcalka1@gmail.com>
2019-04-01 18:46:19 +0200

    Add class CorrectDate

---
+++ WalutyBusinessLogic/CorrectDate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalutyBusinessLogic
+{
+    class CorrectDate
+    {
+        public int SetCorrectDate(int date, string nameCurrecncy)
+        {
+            if()
+            return date;
+        }
+
+    }
+}
