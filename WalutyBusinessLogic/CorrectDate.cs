37e2b8308e5f4b38a424e4b4e6cd1783cb174f46
Przemek Całka <przemekcalka1@gmail.com>
2019-04-02 17:36:45 +0200

    Add method GetCurrencyDateList in CorrectDate.cs

--- WalutyBusinessLogic/CorrectDate.cs
+++ WalutyBusinessLogic/CorrectDate.cs
@@ -1,16 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using WalutyBusinessLogic.LoadingFromFile;
 
 namespace WalutyBusinessLogic
 {
     class CorrectDate
     {
-        public int SetCorrectDate(int date, string nameCurrecncy)
+        public int SetCorrectDate(int dateCurrency, string nameCurrency)
         {
-            if()
-            return date;
+            List<CurrencyRecord> CurrencyDateList = GetCurrencyDateList(nameCurrency);
+            if (dateCurrency < CurrencyDateList[0].Date)
+            {
+                dateCurrency = CurrencyDateList[0].Date;
+            }
+            if (dateCurrency >= CurrencyDateList[0].Date && dateCurrency <= CurrencyDateList[CurrencyDateList.Count-1].Date)
+            {
+                dateCurrency = CurrencyDateList[0].Date;
+            }
+            if (dateCurrency > CurrencyDateList[CurrencyDateList.Count - 1].Date)
+            {
+                dateCurrency = CurrencyDateList[CurrencyDateList.Count - 1].Date;
+            }
+            return dateCurrency;
         }
 
+        private List<CurrencyRecord> GetCurrencyDateList(string nameCurrency)
+        {
+            Loader loader = new Loader();
+            Currency currency = loader.LoadCurrencyFromFile(nameCurrency);
+            List<CurrencyRecord> CurrencyDateList = currency.ListOfRecords;
+            return CurrencyDateList;
+        }
     }
 }
