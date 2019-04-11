659bcb5342644163f19ba1ce584803733de8d855
teder <t.der@wp.pl>
2019-04-12 00:19:33 +0200

    Removing sequences of whitespace

--- WalutyMVCWebApp/Startup.cs
+++ WalutyMVCWebApp/Startup.cs
@@ -29,7 +29,7 @@ namespace WalutyMVCWebApp
             });
 
             services.AddSingleton<ILoader, Loader>();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);           
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
