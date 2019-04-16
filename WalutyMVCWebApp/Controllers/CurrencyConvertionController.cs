7285dd523958f417896ee4bda9233e56a8265b38
Przemek Całka <przemekcalka1@gmail.com>
2019-04-16 22:25:48 +0200

    Change namespace on WalutyMVCWebApp

---
+++ WalutyMVCWebApp/Controllers/CurrencyConvertionController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WalutyMVCWebApp.Controllers
+{
+    public class CurrencyConvertionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
