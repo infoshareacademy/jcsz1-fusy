285618be77e4976d4953e2ea157dee401808bef3
Przemek Całka <przemekcalka1@gmail.com>
2019-04-16 22:02:10 +0200

    Add new file Correctdate.cs and folder services

---
+++ WalutyMVCWebApp/Services/CorrectDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WalutyBusinessLogic.LoadingFromFile;
+
+namespace WalutyMVCWebApp.Services
+{
+    public class CorrectDate
+    {
+        public int SetCorrectDate(int dateCurrency, string nameCurrency)
+        {
+            List<CurrencyRecord> CurrencyDateList = GetCurrencyDateList(nameCurrency);
+            if (CurrencyDateList.Any(c => c.Date == dateCurrency))
+            {
+                return dateCurrency;
+            }
+            else return 0;
+        }
+
+        private List<CurrencyRecord> GetCurrencyDateList(string nameCurrency)
+        {
+            Loader loader = new Loader();
+            Currency currency = loader.LoadCurrencyFromFile(nameCurrency);
+            List<CurrencyRecord> CurrencyDateList = currency.ListOfRecords;
+            return CurrencyDateList;
+        }
+    }
+}
