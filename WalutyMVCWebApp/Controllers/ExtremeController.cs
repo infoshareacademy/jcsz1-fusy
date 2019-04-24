a5664577b34b41d5f7ae783c7bc09299ee17562c
Przemek Całka <przemekcalka1@gmail.com>
2019-04-25 00:46:41 +0200

    Add controller for extrem

---
+++ WalutyMVCWebApp/Controllers/ExtremeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WalutyMVCWebApp.Controllers
+{
+    public class ExtremeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Default action");
+        }
+    }
+}
