d0b2377fafba47f69d2960daa7d3f8fb867e3ea8
Przemek Całka <przemekcalka1@gmail.com>
2019-04-28 08:30:53 +0200

    Add next action

--- WalutyMVCWebApp/Controllers/CurrencyConvertionController.cs
+++ WalutyMVCWebApp/Controllers/CurrencyConvertionController.cs
@@ -2,20 +2,34 @@
 using Microsoft.AspNetCore.Mvc;
 using WalutyBusinessLogic.LoadingFromFile;
 using WalutyMVCWebApp.Services;
+using WalutyMVCWebApp.Models;
+
 namespace WalutyMVCWebApp.Controllers
 {
     public class CurrencyConvertionController : Controller
     {
         private readonly CurrencyConvertionServices _currencyConvertion;
+        private readonly ILoader loader;
 
-        //public CurrencyConvertionController(ILoader loader, string  firstNameCurrency, string secondNameCurrency)
+        //public CurrencyConvertionController(CurrencyConvertionServices currencyConvertionService)
         //{
-        //    _currencyConvertion = new CurrencyConvertionServices(loader, firstNameCurrency, secondNameCurrency);
+        //    _currencyConvertion = currencyConvertionService;
+
         //}
+
 
         public IActionResult Index()
         {
+            var currencyConvertionModel = new CurrencyConvertionModel();
             return View();
         }
+
+        //[HttpPost]
+        //public IActionResult ShowResultCurrencyConvertion(ILoader loader, CurrencyConvertionModel currencyConvertionModel)
+        //{
+        //    var g = new CurrencyConvertionServices(loader,currencyConvertionModel.FirstCurrency, currencyConvertionModel.SecondCurrency);
+            
+        //    return View(g.CalculateAmountForCurrencyConvertion(currencyConvertionModel.AmountFirstCurrency, currencyConvertionModel.Date));
+        //}
     }
 }
