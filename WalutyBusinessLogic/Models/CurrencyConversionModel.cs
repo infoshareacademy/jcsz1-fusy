88aadf3c23e2ac7ce035d233cfdb5c6c8028e492
Przemek Całka <przemekcalka1@gmail.com>
2019-05-13 16:54:12 +0200

    Removal of a typo from the name

---
+++ WalutyBusinessLogic/Models/CurrencyConversionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WalutyBusinessLogic.Models
+{
+    public class CurrencyConversionModel
+    {
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        [StringLength(3, ErrorMessage = "Currency name must have 3 letter. Example: USD")]
+        public string FirstCurrency { get; set; }
+        [Required]
+        [StringLength(3, ErrorMessage = "Currency name must have 3 letter. Example: USD")]
+        public string SecondCurrency { get; set; }
+        [Required]
+        public float AmountFirstCurrency { get; set; }
+        public float AmountSecondCurrency { get; set; }
+    }
+}
---
+++ WalutyMVCWebApp/Controllers/CurrencyConversionController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WalutyBusinessLogic.LoadingFromFile;
+using WalutyBusinessLogic.Models;
+using WalutyBusinessLogic.Services;
+
+namespace WalutyMVCWebApp.Controllers
+{
+    public class CurrencyConversionController : Controller
+    {
+        private readonly CurrencyConversionService _currencyConversionService;
+        public CurrencyConversionController(ILoader loader)
+        {
+            _currencyConversionService = new CurrencyConversionService(loader);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ShowResultCurrencyConversion(CurrencyConversionModel currencyConversionModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Index");
+            }
+            return View(_currencyConversionService.CalculateAmountForCurrencyConversion(currencyConversionModel));
+        }
+    }
+}
