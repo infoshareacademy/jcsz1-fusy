24cba3cc3fc7378aba7c26508f45687d7d5dd0f5
Przemek Całka <przemekcalka1@gmail.com>
2019-05-22 18:35:52 +0200

    Create model and use him in Currencies Comparator

---
+++ WalutyBusinessLogic/Models/CurrenciesComparatorModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WalutyBusinessLogic.Models
+{
+    public class CurrenciesComparatorModel
+    {
+        [Required]
+        [StringLength(3, ErrorMessage = "Currency name must have 3 letter. Example: USD")]
+        public string FirstCurrencyCode { get; set; }
+        [Required]
+        [StringLength(3, ErrorMessage = "Currency name must have 3 letter. Example: USD")]
+        public string SecondCurrencyCode { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        public string ComparatorResult { get; set; }
+    }
+}
