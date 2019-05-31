6e9ea8a01105a09d4bbfa8c50570efb461508010
Przemek Całka <przemekcalka1@gmail.com>
2019-05-31 07:55:07 +0200

    Add validation in model

--- WalutyBusinessLogic/Models/CurrenciesComparatorModel.cs
+++ WalutyBusinessLogic/Models/CurrenciesComparatorModel.cs
@@ -5,14 +5,17 @@ namespace WalutyBusinessLogic.Models
 {
     public class CurrenciesComparatorModel
     {
-        [Required]
+        [Required(ErrorMessage ="Currency is required")]
         [StringLength(3, ErrorMessage = "Currency name must have 3 letter. Example: USD")]
         public string FirstCurrencyCode { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Currency is required")]
         [StringLength(3, ErrorMessage = "Currency name must have 3 letter. Example: USD")]
         public string SecondCurrencyCode { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Date is required")]
         public DateTime Date { get; set; }
+
         public string ComparatorResult { get; set; }
     }
 }
