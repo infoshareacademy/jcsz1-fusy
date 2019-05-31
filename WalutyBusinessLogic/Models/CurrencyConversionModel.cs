4b027d5b3417e7277b5898541bada1b0f8dee01d
Przemek Całka <przemekcalka1@gmail.com>
2019-05-31 08:08:51 +0200

    Add valiation in controller for date

--- WalutyBusinessLogic/Models/CurrencyConversionModel.cs
+++ WalutyBusinessLogic/Models/CurrencyConversionModel.cs
@@ -5,15 +5,17 @@ namespace WalutyBusinessLogic.Models
 {
     public class CurrencyConversionModel
     {
-        [Required]
+        [Required(ErrorMessage = "Date is required")]
         public DateTime Date { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Currency is required")]
         [StringLength(3, ErrorMessage = "Currency name must have 3 letter. Example: USD")]
         public string FirstCurrency { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Currency is required")]
         [StringLength(3, ErrorMessage = "Currency name must have 3 letter. Example: USD")]
         public string SecondCurrency { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Amount currency is required")]
+        [Range (1,10000000)]
         public float AmountFirstCurrency { get; set; }
         public float AmountSecondCurrency { get; set; }
     }
---
+++ WalutyMVCWebApp/Controllers/CurrencyConversionController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WalutyBusinessLogic.LoadingFromFile;
+using WalutyBusinessLogic.Models;
+using WalutyBusinessLogic.Services;
+
+namespace WalutyMVCWebApp.Controllers
+{
+    public class CurrencyConversionController : Controller
+    {
+        private readonly CurrencyConversionService _currencyConversionService;
+        private readonly DateChecker _dateChecker;
+        private readonly DateRange _dateRange;
+        public CurrencyConversionController(ILoader loader)
+        {
+            _currencyConversionService = new CurrencyConversionService(loader);
+            _dateChecker = new DateChecker();
+            _dateRange = new DateRange(loader);
+        }
+
+        public IActionResult CurrencyConversionOfForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ShowResultCurrencyConversion(CurrencyConversionModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("CurrencyConversionOfForm", model);
+            }
+            if (!_dateChecker.CheckingIfDateExistsForTwoCurrencies(model.Date, model.FirstCurrency, model.SecondCurrency))
+            {
+                ViewBag.DateRangeForConversion = _dateRange.GetDateRangeTwoCurrencies(model.FirstCurrency, model.SecondCurrency);
+
+                return View("CurrencyConversionOfForm", model);
+            }
+            return View(_currencyConversionService.CalculateAmountForCurrencyConversion(model));
+        }
+    }
+}
