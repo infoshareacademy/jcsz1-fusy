5660f42686a96c4028a3706adc8fcd35cfc4f667
Przemek Całka <przemekcalka1@gmail.com>
2019-05-31 07:49:47 +0200

    Add controller for Currency comparision and two views

---
+++ WalutyMVCWebApp/Controllers/CurrencyComparisionController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WalutyBusinessLogic.Models;
+using WalutyBusinessLogic.CurrenciesComparision;
+using WalutyBusinessLogic.LoadingFromFile;
+using WalutyBusinessLogic.Services;
+
+namespace WalutyMVCWebApp.Controllers
+{
+    public class CurrencyComparisionController : Controller
+    {
+        private readonly CurrenciesComparator _currenciesComparator;
+        private readonly DateChecker _dateChecker;
+        private readonly DateRange _dateRange;
+
+        public CurrencyComparisionController(ILoader loader)
+        {
+            _currenciesComparator = new CurrenciesComparator(loader);
+            _dateChecker = new DateChecker();
+            _dateRange = new DateRange(loader);
+        }
+
+        public IActionResult CurrencyComparatorOfForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult ShowResultCurrencyComparision(CurrenciesComparatorModel model)
+        {
+            if (!_dateChecker.CheckingIfDateExistsForTwoCurrencies(model.Date, model.FirstCurrencyCode, model.SecondCurrencyCode))
+            {
+                ViewBag.DateRangeForComparison = _dateRange.GetDateRangeTwoCurrencies(model.FirstCurrencyCode, model.SecondCurrencyCode);
+                
+                return View("CurrencyComparatorOfForm", model);
+            }
+            if (!ModelState.IsValid)
+            {
+                return View("CurrencyComparatorOfForm", model);
+            }
+            return View(_currenciesComparator.CompareCurrencies(model));
+        }
+    }
+}
