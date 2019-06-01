8e83c3ae7fcc5323d04c1ed41cfdb6660d4e8f25
Dariusz Jagiellowicz <dariuszjagiellowicz@gmail.com>
2019-06-01 12:16:21 +0200

    Added Scaffolded identity Login/Register/Logout pages

---
+++ WalutyMVCWebApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using WalutyBusinessLogic.LoadingFromFile.DatabaseLoading;
+using WalutyBusinessLogic.Models;
+
+[assembly: HostingStartup(typeof(WalutyMVCWebApp.Areas.Identity.IdentityHostingStartup))]
+namespace WalutyMVCWebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
